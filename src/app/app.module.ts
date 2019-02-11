8009919ac0711403800317474130a07518fc6f3a
Northerchild <osarus-13@hotmail.com>
2019-02-10 20:36:37 -0500

    Se crean los estilos del chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+import { ChatComponent } from './components/chat/chat.component';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule // imports firebase/auth, only needed for auth features,
-  
+    AngularFireAuthModule,// imports firebase/auth, only needed for auth features,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
